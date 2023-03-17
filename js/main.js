ac0a64bdff4c74b385a24ab4d401eb025279f320
Pedro Padilha Neto <pedropadilhan@gmail.com>
2023-03-17 18:45:35 -0300

    swiper

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 19,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
